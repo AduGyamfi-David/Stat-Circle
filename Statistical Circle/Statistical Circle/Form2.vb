89249855d0079016818fc5d277146aa22420ef2e
David Adu-Gyamfi <adugyamfi3141@gmail.com>
2021-07-17 15:10:16 +0100

    Began on creating form to display all point inside and outside circle (testing if I can add points to form)

---
+++ Statistical Circle/Statistical Circle/Form2.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form2
+    Dim Hits() As Panel
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+
+        Dim Stats As New Button With {
+            .Width = 200,
+            .Height = 50,
+            .Location = New Point(Me.Width - 200, Me.Height - 75),
+            .Text = "SEE STATS"}
+
+        AddPoints()
+    End Sub
+
+    Private Sub AddPoints()
+        For i = 1 To 100
+            Dim Hit As New Panel With {
+                .Width = 5,
+                .Height = 5,
+                .Location = New Point(Rnd() * Me.Width, Rnd() * Me.Height),
+                .Parent = Me,
+                .BackColor = Color.Black
+            }
+        Next
+
+    End Sub
+End Class
